6901364f88f0abf11349788d06ccb5244307a179
Taekyun Kim <brandon3194@naver.com>
2024-09-06 15:23:29 +0900

    add: C, R in CRUD

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+diaries = {}
+next_id = 1
+
+@app.route("/diaries", methods=['GET'])
+def get_diaries():
+    return jsonify(list(diaries.values()))
+
+@app.route("/diaries", methods=['POST'])
+def create_diary():
+    global next_id
+    data = request.json
+
+    if 'title' not in data or 'content' not in data:
+        return jsonify({'error': 'Title and content are required'}), 400
+    
+    diary = {
+        'id': next_id,
+        'title': data['title'],
+        'content': data['content']
+    }
+
+    diaries[next_id] = diary
+    next_id += 1
+    return jsonify(diary), 201
+
