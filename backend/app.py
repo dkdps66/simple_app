0715eb578b436bd7c00d109b150824ff9c453a75
Taekyun Kim <kwangjutk@gmail.com>
2024-09-09 08:45:00 +0900

    Add: CRUD test

--- backend/app.py
+++ backend/app.py
@@ -27,3 +27,37 @@ def create_diary():
     next_id += 1
     return jsonify(diary), 201
 
+@app.route("/diaries/<int:diary_id>", methods=['PUT'])
+def update_diary(diary_id):
+    data = request.json
+    diary = diaries.get(diary_id)
+
+    # 만약에 수정할 다이어리가 없다면, Diary not Found 에러 반환, 응답코드 404
+    if diary is None:
+        return jsonify({'error': 'Diary not found'}), 404
+
+    # diary 변수 업데이트
+    if 'title' in data:
+        diary['title'] = data['title']
+
+    if 'content' in data:
+        diary['content'] = data['content']
+
+    # diaries 딕셔너리 전역변수 업데이트
+    diaries[diary_id] = diary
+
+    # json 형식으로 업데이트된 다이어리 반환, 응답코드 200(기본값)
+    return jsonify(diary)
+
+@app.route("/diaries/<int:diary_id>", methods=['DELETE'])
+def delete_diary(diary_id):
+    diary = diaries.get(diary_id)
+
+    # 삭제할 다이어리가 없을 때, 404
+    if diary is None:
+        return jsonify({'error': 'Diary not found'}), 404
+    
+    # 다이어리 제거
+    del diaries[diary_id]
+
+    return jsonify(diary)
