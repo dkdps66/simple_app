f43460ef6622b0630ef536c1dce9075b9df5b1db
Taekyun Kim <kwangjutk@gmail.com>
2024-09-11 15:45:46 +0900

    Add: DynamoDB

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import boto3
 
 app = Flask(__name__)
 CORS(app)
 
-diaries = {}
+session = boto3.Session(profile_name='mfa')
+dynamodb = session.resource('dynamodb')
+table = dynamodb.Table('diaries')
 next_id = 1
 
 @app.route("/diaries", methods=['GET'])
 def get_diaries():
-    return jsonify(list(diaries.values()))
+    items = table.scan()
+    return jsonify(items['Items'])
 
 @app.route("/diaries", methods=['POST'])
 def create_diary():
@@ -25,14 +29,14 @@ def create_diary():
         'content': data['content']
     }
 
-    diaries[next_id] = diary
+    table.put_item(Item=diary)
     next_id += 1
     return jsonify(diary), 201
 
 @app.route("/diaries/<int:diary_id>", methods=['PUT'])
 def update_diary(diary_id):
     data = request.json
-    diary = diaries.get(diary_id)
+    diary = table.get_item(Key={'id': diary_id}).get('Item')
 
     # 만약에 수정할 다이어리가 없다면, Diary not Found 에러 반환, 응답코드 404
     if diary is None:
@@ -46,20 +50,20 @@ def update_diary(diary_id):
         diary['content'] = data['content']
 
     # diaries 딕셔너리 전역변수 업데이트
-    diaries[diary_id] = diary
+    table.put_item(Item=diary)
 
     # json 형식으로 업데이트된 다이어리 반환, 응답코드 200(기본값)
     return jsonify(diary)
 
 @app.route("/diaries/<int:diary_id>", methods=['DELETE'])
 def delete_diary(diary_id):
-    diary = diaries.get(diary_id)
+    diary = table.get_item(Key={'id': diary_id}).get('Item')
 
     # 삭제할 다이어리가 없을 때, 404
     if diary is None:
         return jsonify({'error': 'Diary not found'}), 404
     
     # 다이어리 제거
-    del diaries[diary_id]
-
+    table.delete_item(Key={'id': diary_id})
+    
     return jsonify(diary)
